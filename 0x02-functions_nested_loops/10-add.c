55bc636a6531eb7cff4c68d8d35697ae44607fef
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-08-15 21:47:45 -0700

    some more functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_abs - Check computes the absolute value of an integer.
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
