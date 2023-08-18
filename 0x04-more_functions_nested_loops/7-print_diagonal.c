44af866a4f5823bc11a95974af3d48480e4a3ab2
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-08-17 17:16:25 -0700

    added more tasks on functions and nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,4 +9,23 @@
 
 void print_diagonal(int n)
 {
+	int i;
+	int j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
