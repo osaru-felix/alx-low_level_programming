74711519876cdf2cf6109593be58212f244888bd
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-08-28 09:38:59 -0700

    updated code for betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,10 +10,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s[i] != accept[j]; j++)
-		{		
+		{
 			if (accept[j] == '\0')
 				return (i);
 		}
