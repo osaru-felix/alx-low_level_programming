73ce8f804b3803c57b7ce26f7456e65e178d9bf9
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-09-07 15:33:36 -0700

    added one advanced task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
