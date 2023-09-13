7cfa55d270b9f673fb80c09e8a9828970adc3dc2
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-09-13 06:35:35 -0700

    added task 4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+ * op_add - Function returns the sum of two values
+ * @a: value 1
+ * @b: value 2
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - Function returns the difference of two values
+ * @a: value 1
+ * @b: value 2
+ * Return: Difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function returns the product of two values
+ * @a: value 1
+ * @b: Value 2
+ * Return: Product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function returns the quotient of two values
+ * @a: value 1
+ * @b: value 2
+ * Return: Quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function returns the remainder of two values
+ * @a: value 1
+ * @b: value 2
+ * Return: Remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
