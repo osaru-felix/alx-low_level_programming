9977da12409b813f6980c60d0ff3ad864d3a43cb
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-09-21 14:21:58 -0700

    updated the header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
