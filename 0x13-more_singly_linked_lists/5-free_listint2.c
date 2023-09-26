f6d98b9ab5a98cceaac721797c0b1a7e00a8d247
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-09-26 09:23:14 -0700

    added the task 5-free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(*head);
+	*head = NULL;
+}
