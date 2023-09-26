c3f460a847a454f5ada02caf35b4ebe9efb1334c
Osarumwense Felix <osarumwensefelix@gmail.com>
2023-09-26 10:58:05 -0700

    updated the insert task code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -47,7 +47,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 		*head = new;
-	for (i = 0; i < idx - 1 && tmp != NULL && idx != 0 i++)
+	for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
 		tmp = tmp->next;
 	if (tmp == NULL)
 		return (NULL);
